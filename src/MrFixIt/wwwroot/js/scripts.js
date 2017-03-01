ab9103747061e92600f4cab5fe229703ec192167
Steve Burton <stevevburton@gmail.comgit config --global user.email stevevburton@gmail.comgit config --global user.email stevevburton@gmail.com>
2017-02-28 20:49:29 -0800

    app works, but I can't get the external scripts file to work

--- src/MrFixIt/wwwroot/js/scripts.js
+++ src/MrFixIt/wwwroot/js/scripts.js
@@ -1,1 +1,62 @@
-﻿var test = "it works";
+﻿$(document).ready(function () {
+    $('.claim-view').click(function (event) {
+        event.preventDefault();
+        $.ajax({
+            type: 'POST',
+            dataType: 'json',
+            data: $(this).serialize(),
+            url: '@Url.Action("ClaimDone")',
+            success: function (result) {
+            }
+        });
+    });
+});
+
+$(document).ready(function () {
+    $('.claim-view').click(function (event) {
+        event.preventDefault();
+        $.ajax({
+            type: 'GET',
+            dataType: 'html',
+            data: $(this).serialize(),
+            url: '@Url.Action("Claim")',
+            success: function (result) {
+                $('.display-claim').html(result);
+            }
+        });
+    });
+});
+
+$(document).ready(function () {
+    $('.pending-view').submit(function (event) {
+        event.preventDefault();
+        $.ajax({
+            type: 'POST',
+            dataType: 'json',
+            data: $(this).serialize(),
+            url: '@Url.Action("PendingJob", "Jobs")',
+            success: function (result) {
+                var resultMessage = "<strong>" + result.title + " - " + "</strong>" + result.description;
+                $('.display-pending').html(resultMessage);
+            }
+        });
+    });
+});
+
+$(document).ready(function () {
+    $('.completed-view').submit(function (event) {
+        event.preventDefault();
+        $.ajax({
+            type: 'POST',
+            dataType: 'json',
+            data: $(this).serialize(),
+            url: '@Url.Action("CompletedJob", "Jobs")',
+            success: function (result) {
+                var resultMessage = "<strong>" + result.title + " - " + "</strong>" + result.description;
+                $('.display-completed').html(resultMessage);
+            }
+        });
+    });
+});
+
+
